a6992d5c918b1cc0a167b65e8c4385bf9d58b91e
Shishkov Egor <131530870+AnimeMakartur@users.noreply.github.com>
2026-04-04 16:21:15 +0300

    Bump assembly informational version
    Update AssemblyInformationalVersionAttribute to new build identifier (1.0.0+861cdc6...) and refresh AssemblyInfoInputs.cache to reflect the regenerated Debug/net8.0 build metadata.

--- back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
+++ back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9359e115d30d8125c096ce3cb4e1d3bdd59ef74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+861cdc6542a3c4e1302f9a97f1ed7d3e40f86b0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
